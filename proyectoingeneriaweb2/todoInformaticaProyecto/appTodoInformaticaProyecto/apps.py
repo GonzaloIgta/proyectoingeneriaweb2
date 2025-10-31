a2ec4e8af97034dab14d2bd3e43622ef09ef8f81
Gonzalo Iglesias <gonzalo.iglesias@opendeusto.es>
2025-10-31 09:51:16 +0100

    Delete proyectoingeneriaweb2 directory

--- proyectoingeneriaweb2/todoInformaticaProyecto/appTodoInformaticaProyecto/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApptodoinformaticaproyectoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appTodoInformaticaProyecto'
