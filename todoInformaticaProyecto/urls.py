83c534d92be06b87a21fe7e9ae0e8f662395168a
Gonzalo Iglesias <gonzalo.iglesias@opendeusto.es>
2025-12-08 16:42:26 +0100

    refactorizado

--- todoInformaticaProyecto/urls.py
+++ todoInformaticaProyecto/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include, re_path # Added re_path and included them all
+from django.urls import path, include, re_path
 
 urlpatterns = [
  re_path(r'^i18n/', include('django.conf.urls.i18n')),
